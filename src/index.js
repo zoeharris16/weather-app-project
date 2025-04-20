a03a8d5cfbc045fb19101797a04791a4bd8c69bf
zoeharris16 <zoe.harris16@gmail.com>
2025-04-20 09:55:05 +0100

    created city search functionality using API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
+function updateValue(response) {
+  let temperatureElement = document.querySelector("#temp-value");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function findCity(city) {
+  let apiKey = "ea6b5e905f9afo1d15ta66af3bbd604d";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(updateValue);
+}
+
 function changeCity(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+  findCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", changeCity);
+
+findCity("London");
