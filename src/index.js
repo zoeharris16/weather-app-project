df81ad2ecc8f11a217d65e2ffa83cc1baa764b98
zoeharris16 <zoe.harris16@gmail.com>
2025-04-21 11:49:13 +0100

    created new insertForecast function in js and updated html accordingly

--- src/index.js
+++ src/index.js
@@ -14,21 +14,6 @@ function updateValue(response) {
   temperatureElement.innerHTML = Math.round(temperature);
 }
 
-function findCity(city) {
-  let apiKey = "ea6b5e905f9afo1d15ta66af3bbd604d";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
-  axios.get(apiUrl).then(updateValue);
-}
-
-function changeCity(event) {
-  event.preventDefault();
-  let searchInput = document.querySelector("#search-form-input");
-  findCity(searchInput.value);
-}
-
-let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", changeCity);
-
 function formatDate(date) {
   let minutes = date.getMinutes();
   let hours = date.getHours();
@@ -55,10 +40,46 @@ function formatDate(date) {
   let formattedDay = days[day];
   return `${formattedDay} ${hours}:${minutes}, `;
 }
+function findCity(city) {
+  let apiKey = "ea6b5e905f9afo1d15ta66af3bbd604d";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(updateValue);
+}
+function changeCity(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  findCity(searchInput.value);
+}
+
+function insertForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `<div class="forecast-container">
+          <div class="forecast-date">${day}</div>
+          <div class="forecast-icon">☀️</div>
+          <div class="forecast-temps">
+            <div class="forecast-temp">
+              <strong>15°</strong>
+            </div>
+            <div class="forecast-temp">9°</div>
+          </div>
+        </div>`;
+  });
+
+  forecastElement = document.querySelector("#forecast-data");
+  forecastElement.innerHTML = forecastHtml;
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", changeCity);
 
 let currentDateElement = document.querySelector("#current-date");
 let currentDate = new Date();
-
 currentDateElement.innerHTML = formatDate(currentDate);
 
 findCity("London");
+insertForecast();
